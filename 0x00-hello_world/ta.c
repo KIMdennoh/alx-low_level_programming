8b827fa0bd867f67141e3c72a6bb863a21408be7
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-01 02:11:14 -0700

    statements and loops in c

--- 0x00-hello_world/ta.c
+++ 0x00-hello_world/ta.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: zero success
+ */
 int main(void)
 {
-	fprintf(stderr,"help me here");
-	return (ear);
+	int a = 0, b = 10;
+
+	for (a < b;;)
+	{
+		printf("%d\n", a);
+	}
+	return (0)	
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * printing lowercase alphabets
+ * Return: zero success
+ */
+int main(void)
+{
+	char a;
+
+	for (a=97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}
