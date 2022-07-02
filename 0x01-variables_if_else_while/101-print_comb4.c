dd24a0ecf2d49404b8fd9bb407aa663e8fb45ebe
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-02 01:02:41 -0700

    combine 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - entry for program
+ *
+ * Return: zero for success
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (i < k && k < j)
+				{
+					putchar(i);
+					putchar(k);
+					putchar(j);
+					if (!(i + j + k) == 168)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
