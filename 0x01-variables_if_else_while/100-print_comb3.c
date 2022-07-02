7e7494a6bbf5934759649d5ba4e3eea5a044bd10
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-01 22:41:00 -0700

    change

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,21 +6,49 @@
  */
 int main(void)
 {
-	int x;
+	int a;
 
-	for (x = 1; x < 100; x++)
+	void add()
 	{
-		if (x % 11 == 0)
-		{
-			continue;
-		}
-		if (x < 10)
-		{
-			putchar('0' + x + ',' + ' ');
-			continue;
-		}
-		putchar('0' + x);
-		putchar(',' + ' ');
+		putchar(',');
+		putchar(' ');
 	}
+	int print(int a)
+	{
+		if (a != 0) 
+		{
+			print(a / 10);
+			putchar((a % 10) + '0');
+		}
+		else
+		{
+			add();
+		}
+	}
+	for (a = 1; a < 10; a++)
+	{
+		putchar('0');
+		putchar('0' + a);
+		if (a == 9)
+		{
+			continue;
+		}
+		else
+		{
+		add();
+		}
+	}
+	for (a = 10; a < 90; a++)
+	{
+		if (a % 11 == 0)
+		{
+			continue;
+		}
+		else
+		{
+			print(a);
+		}
+	}
+	putchar('\n');
 	return (0);
 }
