172e43aebb38aa3b0817d627ff383711f8eb6ffb
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 15:34:36 -0700

    test for signswq

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * print_sign - starting point
+ * @n: value with the sign to test
+ * Return - depending on the sign
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
