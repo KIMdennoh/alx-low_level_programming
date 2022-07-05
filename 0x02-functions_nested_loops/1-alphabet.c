4fe63aa9e2e355dea1f519990ab202f657ef4f2e
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 14:15:44 -0700

    changes for lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include"main.h"
 /**
- * print_alphabes - enntry point
+ * print_alphabet - enntry point
  *
  * Return:void data
  */
 void print_alphabet(void)
 {
 	char alph;
+
 	for (alph = 'a'; alph <= 'z'; alph++)
 	{
 		_putchar(alph);
