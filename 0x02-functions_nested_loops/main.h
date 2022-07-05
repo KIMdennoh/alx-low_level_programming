16c3d59b0d257eab900a6bd1a33fdbe746b3f446
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 12:49:20 -0700

    changes for lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,19 +5,3 @@
  */
 void message(void);
 void print_alphabet(void);
-
-/**
- * print_alphabet - write in lowercase
- *
- * Returns:void data
- */
-void print_alphabet(void)
-{
-	int a;
-
-	for (a = 97; a <= 122; a++)
-	{
-		_putchar(a);
-	}
-	_putchar('\n');
-}
