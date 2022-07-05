b08087650b5109cc0f3a2c3bc3b46882f12eab3b
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 15:54:33 -0700

    test for absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * _abs:start the program
+ * @n: the value for checking absolute
+ * Return - 0 or number sign changed
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n*(-1));
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n);
+	}
+}
