54e5c2544e84de270355a0ddce86b188b16d58e5
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 14:28:25 -0700

    ten times a to z

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 	int a;
 	char j;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
