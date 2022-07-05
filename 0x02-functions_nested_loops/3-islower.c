faa4bfb2dac7601f3497e8761dfa6f4130135de6
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 14:50:42 -0700

    ten times a to z

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
  * _islower - starting point
- * @c has the value for comparison
+ * @c:has the value for comparison
  * Return: 1 for lowercase 0 for  not lowercase
  */
 int _islower(int c)
