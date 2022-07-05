8916776d39d28ff4466bc9c94fcd8a92a073bebf
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 12:20:43 -0700

    lowercse with a loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"main"
+/**
+ * main-see program
+ *
+ * Return:alwys 0
+ */
+int main(void)
+{
+	print_alphabets();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,20 @@
  * Return: void
  */
 void message(void);
+void print_alphabets(void);
 
+/**
+ * print_alphabets - write in lowercase
+ *
+ * Returns:void data
+ */
+void print_alphabets(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
