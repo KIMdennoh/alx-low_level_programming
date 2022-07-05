faad1d0435bf43a490cf32f0a197e31d966bb8af
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 09:46:38 -0700

    header files and initialization

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,3 +10,13 @@ int main(void)
 	_putchar();
 	return (0);
 }
+
+void _putchar(void)
+{
+	int a;
+
+	for (a = 0; a < 9; a++)
+	{
+		putchar(sms[a]);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,2 @@
-char _putchar(void)
-{
-	char message[9]={'_','p','u','t','c','h','a','r','\n'};
-	int a;
+void _putchar(void)
 
-	for (a = 0; a < 9; a++)
-	{
-		putchar(message[a]);
-	}
-	return (0);
-}
