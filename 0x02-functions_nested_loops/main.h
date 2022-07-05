ea9fcdb3d329a4612b497d5f88477aee132f5ffc
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 03:39:02 -0700

    header files in c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+char _putchar(void);
+
+char _putchar(void)
+{
+	char message[9]={'_','p','u','t','c','h','a','r','\n'};
+	int a;
+
+	for (a = 0; a < 9; a++)
+	{
+		putchar(message[a]);
+	}
+	return (0);
+}
