805c32769090e64b11afcf5c5b402b7d20abaa48
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-06 11:53:19 -0700

    from x to 98 printing

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include"main.h"
+/**
+ * print_to_98 - see the program
+ *@n:starting point for printing to 98
+ *return: alwys zero
+ */
+void print_to_98(int a)
+{
+	int x;
+
+	if (a < 98)
+	{
+		for (x = a; x <= 98; x++)
+		{
+			_putchar('0' + x);
+		}
+	}
+	else if (a > 98)
+		for (x = a; x >= 98; x--)
+		{
+			_putchar('0' + x)
+		}
+	else
+	{
+		x = a;
+		_putchar('0' + x);
+	}
+	if (x != 98)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
