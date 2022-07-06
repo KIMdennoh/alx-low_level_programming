17a17b729d28d58e984c379803f1d26fa618949a
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-06 14:50:01 -0700

    first 50 fibionics

---
+++ 0x02-functions_nested_loops/102-fibionacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - see program
+ *
+ * Return:zero
+ */
+int main(void)
+{
+	int i;
+	unsigned long long sum = 1, x, y;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (i % 2 = 0)
+		{
+			x = sum;
+		}
+		else
+		{
+			y = sum;
+		}
+		sum = x + y;
+		printf("%llu, ", sum);
+	}
+	printf("\n");
+}
