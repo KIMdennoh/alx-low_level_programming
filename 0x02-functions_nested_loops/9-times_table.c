872ca228ea966a710eb23ef72f0fb793b9d56d12
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 17:26:23 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include"main.h"
+/**
+ * times_table - starting point of the program
+ *
+ * Return: alwys zero
+ */
+void times_table(void)
+{
+	int i,j,k;
+
+	for (i = 0; i <= 9; a++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if (k / 10 == 0)
+			{
+				if (j == 0)
+				{
+				}
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar((c % 10) + '0');
+				}
+				if (j <9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((k /10) + '0');
+				_putchar((k % 10 + '0');
+				if (j < 9)
+				{
+				_putchar(',');
+				_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
