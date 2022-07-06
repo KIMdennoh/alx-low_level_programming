44e9fd12ba9c6b14cd09b6265d907bc9ec7dcc2c
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-05 17:32:48 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 {
 	int i,j,k;
 
-	for (i = 0; i <= 9; a++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
@@ -17,13 +17,14 @@ void times_table(void)
 			{
 				if (j == 0)
 				{
+					_putchar('0');
 				}
 				if (j != 0)
 				{
 					_putchar(' ');
-					_putchar((c % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
-				if (j <9)
+				if (j < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -31,12 +32,12 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar((k /10) + '0');
+				_putchar((k / 10) + '0');
 				_putchar((k % 10 + '0');
 				if (j < 9)
 				{
-				_putchar(',');
-				_putchar(' ');
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
 		}
