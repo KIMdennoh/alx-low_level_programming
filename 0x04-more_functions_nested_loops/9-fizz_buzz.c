1b792df974f29ca9f84d2c766b4b1ee2a6e93f19
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-07 16:57:22 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - fizz buzz program
+ *
+ * Return:zero
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
