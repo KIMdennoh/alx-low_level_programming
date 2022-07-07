6cede3138a9227e65d32a94bf8a182c83fd817ea
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-07 14:25:05 -0700

    numbers to 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * more_numbers - 0 - 14 ten times
+ *
+ * Return:zero
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	if (x = 0; x < 10;  x++)
+	{
+		if (y = 0; y < 15; y++)
+		{
+			_putchar('0' + y);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
