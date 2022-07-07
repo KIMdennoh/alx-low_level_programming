addf33ace68d4729ad40bbc2e1d0c27df3331229
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-07 13:25:24 -0700

    multiply two ints

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.c"
+/**
+ * mul - multiplys two integers
+ * @a:first integer
+ * @b:last integer
+ * Return:0
+ */
+int mul(int a,int b)
+{
+	_putchar('0' + (a*b);
+	_putchar('\n');
+	return (0);
+}
