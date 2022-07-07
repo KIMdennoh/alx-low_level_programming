18e11ade5905c746de0c6413bc6822f2fd1b33ed
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-07 15:32:29 -0700

    just line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include"main.c"
+/**
+ * print_line - see code
+ *@n:number of lines
+ *Return:0
+ */
+void print_line(int n)
+{
+	if ( n != 0)
+	{
+		for (n = 0; n < n; n++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
