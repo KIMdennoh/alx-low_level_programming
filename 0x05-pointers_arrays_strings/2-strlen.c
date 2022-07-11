5f47c78142dd702008585848230c8a600de170d5
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-11 11:00:13 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _strlen - str
+ * @s: the string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+	while (s[f] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
