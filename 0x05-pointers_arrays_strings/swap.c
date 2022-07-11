d5b5163488b6f15f77ded56355460b1e695f9522
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-11 10:45:44 -0700

    swaping two nmbers

---
+++ 0x05-pointers_arrays_strings/swap.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * swap_int(int *a, int *b);
+ * @a: value for first number
+ * @b: value for second number
+ * Return:0
+ */
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	f = *a;
+	*a = *b;
+	*b = f;
+}
