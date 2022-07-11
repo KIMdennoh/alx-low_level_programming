d7ca4d383a20666ed58b91f9f4e8ed545621e9f8
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-11 11:13:16 -0700

    string output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * void _puts(char *str);
+ * @str: pointer to the string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x]; x++)
+	{
+		_putchar (str[n]);
+	}
+	_putchar('\n');
+}
