2891c6a42ee578965ccfd548d0930c7acfb4a4f9
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-11 13:32:35 -0700

    copay a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,11 +3,12 @@
  * _strcpy - copay string to another string
  * @dest:pointer string to be changed
  * @src:pointer to source string
+ * Return:string
  */
 char *_strcpy(char *dest, char *src)
 {
 	int x = 0;
-	
+
 	for (; src[x] != '\0'; x++)
 	{
 		dest[x] = src[x];
