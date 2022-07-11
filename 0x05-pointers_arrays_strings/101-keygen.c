beb4125cb0890fe304754373ac0ad79f2d947d53
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-11 14:05:03 -0700

    random pass

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - generate radom num
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, sum = 0,pas[100];
+
+	strand(time(NULL));
+	for (i = 0; i < 100; i++)
+	{
+		pas[i] = rad() % 78;
+		sum += (pas[i] + '0');
+		putchar(pas[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			j = 2772 - sum -'0';
+			sum += j;
+			putchar (j + '0');
+			break;
+		}
+	}
+	return (0);
+}
