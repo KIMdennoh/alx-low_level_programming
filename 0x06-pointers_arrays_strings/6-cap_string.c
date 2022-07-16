5017cd0862dec4b4fa8ca57fb32797415ecd31a5
KIMdennoh <gitonga.kimanthi@s.karu.ac.ke>
2022-07-16 16:04:27 -0700

    done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * cap_string - see program
+ * @s:pointer to string
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int x, y;
+	char dels[] = " \t\n,;.!?\"(){}";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+			for (y = 0; dels[y] != '\0'; y++)
+			{
+			if (s[x] == dels[y] && s[x + 1] >= 97 && s[x + i] <= 122)
+				s[x + 1] = s[x + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
