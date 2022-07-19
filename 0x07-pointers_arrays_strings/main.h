cf236ce3d8885d6be87641b8a5e599205dc2c916
KIMdennoh <kimanthidennis02@gmail.com>
2022-07-20 01:05:04 +0300

    mem set

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ int _putchar(char);
 void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
