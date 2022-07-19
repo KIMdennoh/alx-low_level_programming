05d6c521fa5ef08fc82f10e9cb0990a50f5aec62
KIMdennoh <kimanthidennis02@gmail.com>
2022-07-19 21:45:06 +0300

    done

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,4 +2,4 @@ void print_diagsums(int *a, int size);
 int _putchar(char);
 void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
-char *_memset(char *s, char b, unsigned int b;
+char *_memset(char *s, char b, unsigned int n;
